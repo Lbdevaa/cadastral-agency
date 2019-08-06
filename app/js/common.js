62071d1e3235fab57a1aa6a6bb48b893d5b2d99a
Merak <mesheryakov97@gmail.com>
2019-08-06 17:00:48 +0500

    fix links and url

--- app/js/common.js
+++ app/js/common.js
@@ -8,12 +8,12 @@ $(function() {
 			touchDrag: false,
 			dots: true,
 			mouseDrag: false, 
-			
+			// smartSpeed: 10,
 			loop: true,
 			navText: ['<img src="../img/left-arrow.svg">','<img src="../img/right-arrow.svg">'],
 			responsive:{
 				0:{
-					stagePadding: 	260,
+					stagePadding: 260,
 					margin: 20, 
 				},
 				1367:{
@@ -24,21 +24,7 @@ $(function() {
 		}); 
 
 	// open gallery
-	// $('.popup-gallery').magnificPopup({
-	// 	delegate: 'a', // child items selector, by clicking on it popup will open
-	// 	type: 'image',  
-	// 	tLoading: 'Loading image #%curr%...',
-	// 	// mainClass: 'mfp-img-mobile',
-	// 	gallery: {
-	// 		enabled: true,
-	// 		navigateByImgClick: true,
-	// 		preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-	// 	},
-	// 	image: {
-	// 		tError: '<a href="%url%">Изображение #%curr%</a> недоступно.',
-			
-	// 	}
-	//   });
+ 
 	$('.popup-gallery').magnificPopup({
 		type: 'image',
 		delegate: 'a',
@@ -68,11 +54,11 @@ $(function() {
 
 		for(var i = 0; i < owlSlideItem.length; i++){
 			owlSlideItem[i].style.height = slideHeight + 'px';
-			owlSlideItem[i].style.margin = "50px 0 0 0"; 
+			owlSlideItem[i].style.margin = "50px 0 0 0";  
 		} 
 		var activeOwlSlideItem = document.querySelector('.owl-item.active .main-carousel__bg');
 		activeOwlSlideItem.style.height = activeSlideHeight + 'px';
-		activeOwlSlideItem.style.margin = 0;
+		activeOwlSlideItem.style.margin = 0; 
 	};setMainOwlHeight();
 
 	$('.main-carousel').on('translated.owl.carousel', setMainOwlHeight);
